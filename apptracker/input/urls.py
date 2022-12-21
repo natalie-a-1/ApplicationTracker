84a90728f2ce854ecfdf9994ae152a619f547a01
nataliehill <natalie.a.hill-1@ou.edu>
2022-12-21 16:21:43 -0600

    Changed ui

--- apptracker/input/urls.py
+++ apptracker/input/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('applications/', views.ApplicationListView.as_view(), name='applications'),
 ]
--- apptracker/input/views.py
+++ apptracker/input/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
 from .models import Application
 
@@ -10,3 +10,10 @@ def index(request):
         'Application': num_applications
     }
     return render(request, 'index.html', context=context)
+
+
+class ApplicationListView(generic.ListView):
+    model = Application
+    context_object_name = 'application_list'  # your own name for the list as a template variable
+    queryset = Application.objects.all()  # Get 5 books containing the title war
+    template_name = 'application/application_list.html'
